e563d8ff7375f90c38cd6942ff52fbd2adb15c36
Ryan <mhogar98@gmail.com>
2021-10-31 17:54:19 -0400

    Convert actors from Node2D to Controls

--- Kits/Base/Actions/Tweens/Fades/FadeOut.gd
+++ Kits/Base/Actions/Tweens/Fades/FadeOut.gd
@@ -5,9 +5,9 @@ export var displacement : Vector2 = Vector2(0, 0)
 
 
 func init_tweens():
-	var start_pos : Vector2 = actor.position
+	var start_pos : Vector2 = actor.rect_position
 	tween.interpolate_property(
-		actor, "position", start_pos, start_pos+displacement, duration, transition_type, Tween.EASE_IN
+		actor, "rect_position", start_pos, start_pos+displacement, duration, transition_type, Tween.EASE_IN
 	)
 	
 	tween.interpolate_property(
